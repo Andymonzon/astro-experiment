c6c373cdf095b0cbb7313ae211509f9ae823a7d7
Andymonzon <valentinomonzonandres@gmail.com>
2024-06-21 12:27:08 -0300

    refactor: Update API result typing

--- src/models/apiRickAndMorty.model.ts
+++ src/models/apiRickAndMorty.model.ts
@@ -1,4 +1,4 @@
-export interface ApiResult {
+export interface APIResult {
   info: Info;
   results: Character[];
 }
@@ -13,9 +13,36 @@ export interface Info {
 export interface Character {
   id: number;
   name: string;
+  status: Status;
+  species: Species;
   type: string;
-  dimension: string;
-  residents: string[];
+  gender: Gender;
+  origin: Location;
+  location: Location;
+  image: string;
+  episode: string[];
   url: string;
   created: Date;
+}
+
+export enum Gender {
+  Female = "Female",
+  Male = "Male",
+  Unknown = "unknown",
+}
+
+export interface Location {
+  name: string;
+  url: string;
+}
+
+export enum Species {
+  Alien = "Alien",
+  Human = "Human",
+}
+
+export enum Status {
+  Alive = "Alive",
+  Dead = "Dead",
+  Unknown = "unknown",
 }
